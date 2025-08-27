acd511e0cce876a77df6e02043b787ee8494472e
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-08-27 13:32:01 +0200

    Inizializza Repo

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Chiedere erà e km da percorrere
+const age = parseInt(prompt("Quanti anni hai?"));
+const distance = parseInt( prompt("Quanti chilometri vuoi percorrere?"));
+
+console.log(age, distance);
+
+
